c34b317bf03dd9897e0a5e65f470fbf031fd3a56
amyheather <a.heather2@exeter.ac.uk>
2024-02-21 15:59:17 +0000

    Generated who took part page

--- pages/2_Who took part.py
+++ pages/2_Who took part.py
@@ -1,11 +1,35 @@
 import streamlit as st
-from utilities.page_setup import page_setup, page_footer
+from kailo_beewell_dashboard.page_setup import (
+    page_setup, page_footer, blank_lines)
+from kailo_beewell_dashboard.reshape_data import get_school_size
+from kailo_beewell_dashboard.who_took_part import (
+    create_demographic_page_intro, demographic_plots)
+import pandas as pd
 
-page_setup()
+page_setup('symbol')
 
 # Set school
 st.session_state.school = 'School A'
 
-st.title('Who took part?')
+# Use test=True to indent text, which is basically just to prevent me from
+# having to redo the line indentation once have added check_password()
+test = True
+if test:
 
-page_footer(st.session_state.school)
+    # Import data (will need to change this to import from TIDB cloud)
+    dem_prop = pd.read_csv('data/survey_data/aggregate_demographic.csv')
+    counts = pd.read_csv('data/survey_data/overall_counts.csv')
+
+    # Get total pupil number
+    school_size = get_school_size(counts, st.session_state.school, 'symbol')
+
+    # Write title and introduction
+    create_demographic_page_intro(school_size)
+    blank_lines(1)
+
+    # Create the figures (with their titles and descriptions)
+    dem_prop['plot_group'] = dem_prop['measure']
+    demographic_plots(dem_prop, st.session_state.school,
+                      chosen_group='For your school', survey_type='symbol')
+
+    page_footer(st.session_state.school)
